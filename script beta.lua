bc0c3fbf6ae4926503451e33ddc9623816bb4cd6
accountnop <84644622+accountnop@users.noreply.github.com>
2022-01-22 20:28:36 -0300

    Create script beta.lua

---
+++ script beta.lua
@@ -0,0 +1,10 @@
+--[[
+Games:
+Blox Fruits (beta)
+Anime Clicker (Beta)
+Phantom Forces
+Pet Simu
+Mad city 
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/accountnop/kial/main/loader"))()
