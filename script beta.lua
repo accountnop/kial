3ea1526c0663a820c45f46b04894ba1de0166254
accountnop <84644622+accountnop@users.noreply.github.com>
2022-01-30 16:21:59 -0300

    Update script beta.lua

--- script beta.lua
+++ script beta.lua
@@ -5,6 +5,8 @@ Anime Clicker (Beta)
 Phantom Forces
 Pet Simu
 Mad city 
+strucid
+No-Scope Arcade
 --]]
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/accountnop/kial/main/loader"))()
